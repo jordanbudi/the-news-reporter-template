9730a5af5c01d1d0f7c4429e24f5d1e3cd3dab28
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-05-23 19:59:45 +0000

    built out folder structure and menu

---
+++ fix.sh
@@ -0,0 +1,51 @@
+# only run this if you need to update 
+# the article.html
+# files across all folders in articles folder
+
+cp article.html articles/nutrition/article1.html
+cp article.html articles/nutrition/article2.html
+cp article.html articles/nutrition/article3.html
+cp article.html articles/nutrition/article4.html
+cp article.html articles/nutrition/article5.html
+
+cp article.html articles/workouts/article1.html
+cp article.html articles/workouts/article2.html
+cp article.html articles/workouts/article3.html
+cp article.html articles/workouts/article4.html
+cp article.html articles/workouts/article5.html
+
+cp article.html articles/sports/article1.html
+cp article.html articles/sports/article2.html
+cp article.html articles/sports/article3.html
+cp article.html articles/sports/article4.html
+cp article.html articles/sports/article5.html
+
+cp article.html articles/technology/article1.html
+cp article.html articles/technology/article2.html
+cp article.html articles/technology/article3.html
+cp article.html articles/technology/article4.html
+cp article.html articles/technology/article5.html
+
+cp article.html articles/fashion/article1.html
+cp article.html articles/fashion/article2.html
+cp article.html articles/fashion/article3.html
+cp article.html articles/fashion/article4.html
+cp article.html articles/fashion/article5.html
+
+cp article.html articles/travel/article1.html
+cp article.html articles/travel/article2.html
+cp article.html articles/travel/article3.html
+cp article.html articles/travel/article4.html
+cp article.html articles/travel/article5.html
+
+cp article.html articles/mental-health/article1.html
+cp article.html articles/mental-health/article2.html
+cp article.html articles/mental-health/article3.html
+cp article.html articles/mental-health/article4.html
+cp article.html articles/mental-health/article5.html
+
+cp article.html articles/lifestyle/article1.html
+cp article.html articles/lifestyle/article2.html
+cp article.html articles/lifestyle/article3.html
+cp article.html articles/lifestyle/article4.html
+cp article.html articles/lifestyle/article5.html
